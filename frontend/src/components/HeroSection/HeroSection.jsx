7749e49db1a83f8a81762c3b019133d69d4717bf
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-02 22:14:42 +0300

    HeroSection > изменен bg

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -1,8 +1,24 @@
 import styles from './HeroSection.module.scss';
+import cloudBgMp4 from '../../images/background_hero/cloud_bg.mp4';
 
 export default function HeroSection () {
   return (
     <section className={styles.hero}>
+      <video
+        className={styles.backgroundVideo}
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source
+          src={cloudBgMp4}
+          type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+      <div className={styles.heroContent}>
+
+      </div>
     </section>
   );
 }
