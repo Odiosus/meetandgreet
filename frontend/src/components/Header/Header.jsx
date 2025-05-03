b56cb51af1069455ed9a52f1fa81b150bba25590
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-03 18:34:12 +0300

    Header > добавлено лого

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
 import styles from './Header.module.scss';
+import logo from '../../images/logo.svg';
+
 
 export default function Header () {
   return (
     <>
       <header className={styles.header}>
+        <div className={styles.header__body}>
+          <a
+            href="/"
+            rel="noopener noreferrer"
+            aria-label={'Home'}
+            title={'Home'}
+          >
+            <img
+              src={logo}
+              alt=""
+              width={171}
+              height={35}
+            />
+          </a>
+        </div>
       </header>
     </>
   );
