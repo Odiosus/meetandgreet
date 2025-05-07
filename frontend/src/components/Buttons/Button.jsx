7a89fc4019a6d18dc8960fec565e873a058ebea7
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-07 15:16:38 +0300

    Button > добавлен пропс для передачи доп. классов для кнопок

--- frontend/src/components/Buttons/Button.jsx
+++ frontend/src/components/Buttons/Button.jsx
@@ -3,23 +3,23 @@ import styles from "./Button.module.scss";
 
 /**
  * Компонент кнопки.
- * @function
  * @param {Object} props - Объект с пропсами.
  * @param {string} [props.text] - Текст кнопки.
- * @param {string} [props.to] - Если указан, то кнопка будет ссылкой на указанный url.
+ * @param {string} [props.to] - Если указан, то кнопка будет ссылкой на указанный URL.
  * @param {function} [props.onClick] - Функция, которая будет вызвана при клике на кнопку.
  * @param {ReactNode} [props.icon] - Иконка, которая будет отображаться на кнопке.
  * @param {boolean} [props.noBackground] - Если true, то кнопка будет иметь прозрачный фон.
+ * @param {string} [props.className] - Дополнительные классы для кнопки.
  * @returns {ReactComponent} Компонент кнопки.
  */
-export default function Button ({text, to, onClick, icon, noBackground}) {
-  const className = `${styles.button} ${noBackground ? styles['button-no-background'] : ''}`
+export default function Button ({text, to, onClick, icon, noBackground, className = ''}) {
+  const baseClassName = `${styles.button} ${noBackground ? styles['button-no-background'] : ''} ${className}`
 
   if (to) {
     return (
       <Link
         to={to}
-        className={className}
+        className={baseClassName}
       >
         {icon && <span className={styles.icon}>{icon}</span>}
         {text}
@@ -28,7 +28,7 @@ export default function Button ({text, to, onClick, icon, noBackground}) {
   }
   return (
     <button
-      className={className}
+      className={baseClassName}
       onClick={onClick}
     >
       {icon && <span className={styles.icon}>{icon}</span>}
