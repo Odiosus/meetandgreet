8b3f8c60d58afcb2899b3461a7e12fa8236132a8
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-07 15:14:36 +0300

    обернул прилодение в контекст LanguageContext.jsx > LanguageProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,29 +3,32 @@ import Home from "./pages/Home.jsx";
 import FastBooking from "./pages/FastBooking.jsx";
 import Services from "./pages/Services.jsx";
 import Travelers from "./pages/Travelers.jsx";
+import { LanguageProvider } from "./context/LanguageContext.jsx";
 
 function App () {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Home/>}
-        />
-        <Route
-          path={'/fast-booking'}
-          element={<FastBooking/>}
-        />
-        <Route
-          path={'/services'}
-          element={<Services/>}
-        />
-        <Route
-          path={'/travelers'}
-          element={<Travelers/>}
-        />
-      </Routes>
-    </Router>
+    <LanguageProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home/>}
+          />
+          <Route
+            path={'/fast-booking'}
+            element={<FastBooking/>}
+          />
+          <Route
+            path={'/services'}
+            element={<Services/>}
+          />
+          <Route
+            path={'/travelers'}
+            element={<Travelers/>}
+          />
+        </Routes>
+      </Router>
+    </LanguageProvider>
   );
 }
 
