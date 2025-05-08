9ccb1969dd706a4228e56938aa3abfb0d437a60d
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-08 11:43:50 +0300

    VideoBackground > добавлен статический фон перед загрузкой и если видео не может быть воспроизведено

--- frontend/src/components/VideoBackground/VideoBackground.jsx
+++ frontend/src/components/VideoBackground/VideoBackground.jsx
@@ -1,16 +1,50 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "./VideoBackground.module.scss";
 import cloudBgMp4 from "../../images/background_hero/cloud_bg.mp4";
+import cloudBgImage from "../../images/background_hero/cloud_bg.jpg";
 
 
 export default function VideoBackground ({children}) {
+  const videoRef = useRef( null );
+  const [showVideo, setShowVideo] = useState( false )
+
+  useEffect( () => {
+    const video = videoRef.current;
+
+    const handleCanPlay = () => {
+      setShowVideo( true );
+      video.play().catch( () => {
+        setShowVideo( false );
+      } );
+    };
+
+    if (video) {
+      video.addEventListener( 'canplay', handleCanPlay );
+      video.load();
+    }
+
+    return () => {
+      if (video) {
+        video.removeEventListener( 'canplay', handleCanPlay );
+      }
+    }
+  }, [] );
+
   return (
     <div className={styles.videoContainer}>
+      <img
+        className={styles.backgroundImage}
+        src={cloudBgImage}
+        alt="Background"
+      />
       <video
-        className={styles.backgroundVideo}
-        autoPlay
+        ref={videoRef}
+        className={`${styles.backgroundVideo} ${showVideo ? styles.visible : ''}`}
+        /*autoPlay*/
         muted
         loop
         playsInline
+        preload="auto"
       >
         <source
           src={cloudBgMp4}
