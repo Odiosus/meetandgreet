c12cf8b09dbc9a927f120937b583d76dcacc4ca9
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-08 12:04:15 +0300

    VideoBackground > Doc

--- frontend/src/components/VideoBackground/VideoBackground.jsx
+++ frontend/src/components/VideoBackground/VideoBackground.jsx
@@ -4,13 +4,34 @@ import cloudBgMp4 from "../../images/background_hero/cloud_bg.mp4";
 import cloudBgImage from "../../images/background_hero/cloud_bg.jpg";
 
 
+/**
+ * Компонент видеофона на странице
+ * @param {React.ReactElement} children - контент, который будет отображаться поверх видео
+ * @returns {React.ReactElement} - компонент видеофона
+ */
 export default function VideoBackground ({children}) {
+  /**
+   * Ссылка на тег video
+   * @type {React.RefObject<HTMLVideoElement>}
+   */
   const videoRef = useRef( null );
-  const [showVideo, setShowVideo] = useState( false )
 
+  /**
+   * Флаг, который указывает, можно ли отображать видео
+   * @type {boolean}
+   */
+  const [showVideo, setShowVideo] = useState( false );
+
+  /**
+   * Устанавливает прослушку события canplay на видео
+   * @param {HTMLVideoElement} video - видео
+   */
   useEffect( () => {
     const video = videoRef.current;
 
+    /**
+     * Обработчик события canplay
+     */
     const handleCanPlay = () => {
       setShowVideo( true );
       video.play().catch( () => {
@@ -40,7 +61,6 @@ export default function VideoBackground ({children}) {
       <video
         ref={videoRef}
         className={`${styles.backgroundVideo} ${showVideo ? styles.visible : ''}`}
-        /*autoPlay*/
         muted
         loop
         playsInline
