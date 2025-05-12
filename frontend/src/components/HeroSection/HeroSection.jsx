546023dea951256a3b55fef292452dfd635283c6
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-12 23:06:25 +0300

    Hero > HeroSection

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -3,9 +3,9 @@ import styles from './HeroSection.module.scss';
 export default function HeroSection () {
   return (
     <section className={styles.hero}>
-
-      <div className={styles.heroContent}>
-
+      <div className={styles.hero__heroContent}>
+        <h1>VIP SERVICES</h1>
+        <h3>AT ALL AIRPORTS AROUND THE WORLD</h3>
       </div>
     </section>
   );
