06980caac33ed110afb484e1d9bdd9cf287e5a96
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-13 12:40:12 +0300

    HeroSection > декомпозиция

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -1,11 +1,16 @@
 import styles from './HeroSection.module.scss';
+import AccentHeading from "../AccentHeading/AccentHeading.jsx";
 
 export default function HeroSection () {
   return (
     <section className={styles.hero}>
       <div className={styles.hero__heroContent}>
         <h1>VIP SERVICES</h1>
-        <h3>AT ALL AIRPORTS AROUND THE WORLD</h3>
+        <AccentHeading
+          tag={'h3'}
+          accentWords={['AROUND']}>
+          AT ALL AIRPORTS AROUND THE WORLD
+        </AccentHeading>
       </div>
     </section>
   );
