e8db65e66bf724be0076f62005d5d6bbcde24a5f
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-13 18:52:47 +0300

    HeroSection > данные для формы

---
+++ frontend/src/data/airports.js
@@ -0,0 +1,8 @@
+export const airports = [
+  {name: 'Domodedovo', code: 'DME', city: 'Moscow, Russia'},
+  {name: 'Sheremetyevo', code: 'SVO', city: 'Moscow, Russia'},
+  {name: 'Vnukovo', code: 'VKO', city: 'Moscow, Russia'},
+  {name: 'Heathrow', code: 'LHR', city: 'London, UK'},
+  {name: 'Charles de Gaulle', code: 'CDG', city: 'Paris, France'},
+  {name: 'John F. Kennedy', code: 'JFK', city: 'New York, USA'},
+];
