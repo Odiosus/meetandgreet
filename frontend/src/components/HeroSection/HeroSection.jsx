6c0257fe4644f4fc1ff758cc069b38f9e4110e77
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-14 13:44:31 +0300

    HeroSection > подключен компонент AirportSearch

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -1,16 +1,18 @@
 import styles from './HeroSection.module.scss';
 import AccentHeading from "../AccentHeading/AccentHeading.jsx";
+import AirportSearch from "../AirportSearch/AirportSearch.jsx";
 
 export default function HeroSection () {
   return (
     <section className={styles.hero}>
       <div className={styles.hero__heroContent}>
-        <h1>VIP SERVICES</h1>
+        <h1 className={styles.hero__heroContent_title}>VIP SERVICES</h1>
         <AccentHeading
           tag={'h3'}
           accentWords={['AROUND']}>
           AT ALL AIRPORTS AROUND THE WORLD
         </AccentHeading>
+        <AirportSearch />
       </div>
     </section>
   );
