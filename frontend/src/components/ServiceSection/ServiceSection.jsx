23e043427147b98306e9fbee3b60fafe66fbc363
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-05-19 15:44:54 +0300

    Home > ServiceSection > добавлен заголовок

--- frontend/src/components/ServiceSection/ServiceSection.jsx
+++ frontend/src/components/ServiceSection/ServiceSection.jsx
@@ -1,10 +1,18 @@
-import styles from './ServiceSection.module.scss'
-
+import styles from './ServiceSection.module.scss';
+import AccentHeading from "../AccentHeading/AccentHeading.jsx";
 
 export default function ServiceSection () {
   return (
     <section className={styles.serviceSection}>
-      ServiceSection
+      <div className={styles.serviceSection__title}>
+        <AccentHeading
+          tag={'h2'}
+          accentWords={['SERVICES', 'AROUND', 'THAN']}>
+          OUR SERVICES PRESENTED IN MORE
+          THAN 1000 AIRPORTS AROUND THE WORLD
+        </AccentHeading>
+      </div>
+
     </section>
   );
 }
